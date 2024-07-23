b1788c790d53207dbb0dd4b2afe02ad509b8257a
Aeriel <aerielellisk5@gmail.com>
2024-07-22 20:46:54 -0700

    creating the docker files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server: {
+  //   host: true, // This will make the server accessible on the network
+  //   port: 5173, // Specify the port if needed
+  // },
 })
